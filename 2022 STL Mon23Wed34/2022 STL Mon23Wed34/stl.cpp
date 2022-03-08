cc2105d2d19b6a74835b764091e1ee5595e49173
Taegrim <aopo3948@gmail.com>
2022-03-08 23:54:42 +0900

    c++ 복습
    operator<<
    template

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -11,8 +11,8 @@
 
 //[문제] 다음 main을 수정없이 실행되는 템플릿 함수를 작성하라
 
-template<class T>
-void change(T &a, T &b)
+template<typename T>
+void change(T& a, T& b)
 {
 	T temp{ a };
 	a = b;
@@ -21,25 +21,40 @@ void change(T &a, T &b)
 
 class Dog {
 public:
-	Dog(std::string, int) {}
-private:
+	Dog(std::string str, int num) : name{ str }, age{ num } {}
+	
 
+	friend std::ostream& operator<<(std::ostream& , const Dog&);
+private:
+	std::string name;
+	int age;
 };
+
+std::ostream& operator<<(std::ostream& os, const Dog& dog)
+{
+	os << "이름 : " << dog.name << ",  나이 : " << dog.age << std::endl;
+	return os;
+}
 
 int main()
 {
-	/*
+	
 	int a{ 1 }, b{ 2 };
 	change(a, b);
 
-	std::string c = "STL"s;
-	std::string d = "C++"s;
+	std::cout << a << ", " << b << std::endl;
+
+	std::string c = "STL";
+	std::string d = "C++ Programming";
 	change(c, d);
 
-	Dog e{ "코코"s, 3 };
-	Dog f{ "댕댕이"s, 2 };
+	std::cout << c << ", " << d << std::endl;
+
+	Dog e{ "코코", 3 };
+	Dog f{ "댕댕이", 2 };
 	change(e, f);
-	*/
+	
+	std::cout << e << f << std::endl;
 
 	save("stl.cpp");
 }
