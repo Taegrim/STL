e15beda4fd3b75a876b7781f81adb52242863e5a
Taegrim <aopo3948@gmail.com>
2022-03-15 23:06:32 +0900

    File input/output Test
    숫자 1000개 랜덤생성 하면서 파일에 출력
    출력한 파일을 다시 읽어서 콘솔창에 띄움
    파일 입출력 테스트

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -8,11 +8,15 @@
 // 숙제 - 파일 입출력 알아보기
 //-----------------------------------------------------------------------------
 
+#include <fstream>
+#include <format>
+
 #include <iostream>
 #include <random>
 #include "save.h"
 
-std::default_random_engine dre;
+std::random_device rd;
+std::default_random_engine dre(rd());
 std::uniform_int_distribution<int> uid;
 
 // [문제] 엔진과 분포를 이용하여 생성한 int값 1000개를
@@ -22,10 +26,67 @@ std::uniform_int_distribution<int> uid;
 int main()
 // -------
 {
+	// c++이 기본적으로 ANSI 방식으로 텍스트 파일을 읽음
+	// 따라서 한글이 있다면 UFT-8 방식으로 읽는다고 명시해야함
+	//std::locale::global(std::locale("ko_KR.UTF-8"));
 
-	for (int i = 0; i < 1'000; ++i)
-		std::cout << uid(dre) << "\t";
+	std::ofstream out("test.txt");
+
+	if (out.is_open()) {
+		for (int i = 0; i < 100; ++i) {
+			for (int j = 0; j < 10; ++j)
+				out << std::format("{:14}", uid(dre));
+			out << std::endl;
+		}
+	}
+
+	std::ifstream in("test.txt");
 	
-	save("stl.cpp");
+
+	// 찾아본 방식
+
+	// 1. 한번에 읽기
+
+	/*std::string s;
+
+	if (in.is_open()) {
+		in.seekg(0, std::ios::end);
+		int size = in.tellg();
+
+		s.resize(size);
+		in.seekg(0, std::ios::beg);
+
+		in.read(&s[0], size);
+		std::cout << s << std::endl;
+
+	}
+	else {
+		std::cout << "파일을 찾을 수 없습니다! " << std::endl;
+	}*/
+
+
+	// 2. 한줄씩 읽어오기
+
+	/*if (!in.is_open()) {
+		std::cout << "파일을 찾을 수 없습니다! " << std::endl;
+		return 0;
+	}
+	std::string s2;
+	while (in) {
+		getline(in, s2);
+		std::cout << s2 << std::endl;
+	}*/
+
+
+
+	
+	// 벡터를 만들면서 in 파일의 내용을 다 읽어온다.  (STL 컨테이너)
+	std::vector<char> v{ std::istreambuf_iterator<char>{in}, {} };
+
+	// 벡터로 읽어온 파일을 출력한다
+	for (auto d : v)
+		std::cout << d;
+
+	//save("stl.cpp");
 }
  
