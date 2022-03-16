c9649bd65641e462cbe203a277adb16d711e7edf
Taegrim <aopo3948@gmail.com>
2022-03-16 13:21:37 +0900

    FIle input/output
    바이너리 모드 입출력
    파일 읽기, 쓰기
    동적 메모리 할당

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,92 +1,50 @@
 //-----------------------------------------------------------------------------
-// 2022. 3. 14 월23						월23 수34				(2주 1일)
+// 2022. 3. 16 수34						월23 수34				(2주 2일)
 // 
-// 많은 수의 자료를 다루는 연습
-// 1. int -> class
-//    int 1000개를 파일에 저장하기
+// 파일 입출력은 더 이상 알아볼 것이 없다
+//	 text / binary  mod 와   read / write 와  >> , <<  의 조합말곤 없기 떄문이다.
 // 
-// 숙제 - 파일 입출력 알아보기
+// - 파일 입출력 복습 할 것
+// 
+// 다음 시간 : 메모리
+//			   class 입출력
+// 
+//  숙제 - 동적 메모리 할당
 //-----------------------------------------------------------------------------
 
-#include <fstream>
-#include <format>
-
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <random>
 #include "save.h"
 
-std::random_device rd;
-std::default_random_engine dre(rd());
-std::uniform_int_distribution<int> uid;
+// [문제] 사용자가 원하는 갯수만큼 int를 저장할 메모리를 만들어라
+// 그 메모리를 1부터 증가하는 int 값으로 채워라
 
-// [문제] 엔진과 분포를 이용하여 생성한 int값 1000개를
-// 파일 "int 1000개.txt"에 저장하라.
+std::random_device rd;
+std::default_random_engine dre;
+std::uniform_int_distribution uid;
 
 // -------
 int main()
 // -------
 {
-	// c++이 기본적으로 ANSI 방식으로 텍스트 파일을 읽음
-	// 따라서 한글이 있다면 UFT-8 방식으로 읽는다고 명시해야함
-	//std::locale::global(std::locale("ko_KR.UTF-8"));
+	int num;
+	std::cout << "몇 개의 int 를 원하십니까? ";
+	std::cin >> num;
 
-	std::ofstream out("test.txt");
+	int* arr = new int[num];
 
-	if (out.is_open()) {
-		for (int i = 0; i < 100; ++i) {
-			for (int j = 0; j < 10; ++j)
-				out << std::format("{:14}", uid(dre));
-			out << std::endl;
-		}
-	}
+	for (int i{}; i < num; ++i)
+		arr[i] = i + 1;
 
-	std::ifstream in("test.txt");
-	
+	int sum = 0;
+	for (int i{}; i < num; ++i)
+		sum += arr[i];
 
-	// 찾아본 방식
+	std::cout << sum;
 
-	// 1. 한번에 읽기
-
-	/*std::string s;
-
-	if (in.is_open()) {
-		in.seekg(0, std::ios::end);
-		int size = in.tellg();
-
-		s.resize(size);
-		in.seekg(0, std::ios::beg);
-
-		in.read(&s[0], size);
-		std::cout << s << std::endl;
-
-	}
-	else {
-		std::cout << "파일을 찾을 수 없습니다! " << std::endl;
-	}*/
-
-
-	// 2. 한줄씩 읽어오기
-
-	/*if (!in.is_open()) {
-		std::cout << "파일을 찾을 수 없습니다! " << std::endl;
-		return 0;
-	}
-	std::string s2;
-	while (in) {
-		getline(in, s2);
-		std::cout << s2 << std::endl;
-	}*/
-
-
-
-	
-	// 벡터를 만들면서 in 파일의 내용을 다 읽어온다.  (STL 컨테이너)
-	std::vector<char> v{ std::istreambuf_iterator<char>{in}, {} };
-
-	// 벡터로 읽어온 파일을 출력한다
-	for (auto d : v)
-		std::cout << d;
+	delete[] arr;
 
 	//save("stl.cpp");
 }
- 
