981a95751acda24e78e651ccbe506285c64f4044
Taegrim <aopo3948@gmail.com>
2022-05-16 12:20:50 +0900

    map
    map 사용법

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// 2022. 5. 11  수34						월23 수34				 (10주 2일)
+// 2022. 5. 16  월23						월23 수34				 (11주 1일)
 // 
 // 컨테이너 - 다른 객체를 저장하는 객체
 // Associative Container
@@ -8,11 +8,10 @@
 // Unordered Associative Container
 // 
 // C++20 span
+// 6월 15일 (15주 2일 / 수요일) 기말 시험
 //-----------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
-#include <map>
-#include <vector>
 #include "save.h"
 #include "STRING.h"
 
@@ -22,18 +21,7 @@ extern bool 관찰;
 int main()
 // -------
 {
-	//[문제] "stl.cpp"의 소문자 갯수를 세서 출력하라
-	std::ifstream in{ "stl.cpp" };
-
-	std::map<char, int> cim;
-
-	char c;
-	while (in >> c)
-		if ( std::islower(c))
-			cim[c]++;
-	
-	for (auto [c, num] : cim)
-		std::cout << c << " - " << num << std::endl;
-
 	//save("stl.cpp");
+	
+	
 } 
