eab245448f22ce647459da331b45d1c3a47de527
Taegrim <aopo3948@gmail.com>
2022-05-30 12:21:40 +0900

    concepts, iterator, template
    C++20 에서 템플릿 함수에 인자를 한정하는 방법
    concept, iterator_traits

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,35 +1,56 @@
 //-----------------------------------------------------------------------------
-// 2022. 5. 25  수34						월23 수34				 (12주 2일)
+// 2022. 5. 30  월23						월23 수34				 (13주 1일)
+// 
+// C++20의 4가지 핵심 변화
+// range / module / concepts / coroutine
+// 
+// type-traits
+// range - view
 // 
 // 
+// 6월 6일						수업해야 함
 // 6월 15일 (15주 2일 / 수요일) 기말 시험
 // 6월 20일 (16주 1일 / 월요일) 끝
 //-----------------------------------------------------------------------------
 #include <iostream>
-#include <vector>
+#include <ranges>
+#include <random>
 #include "save.h"
 #include "STRING.h"
 
-template<typename Iter>
-long long dist(Iter b, Iter e)
-{
-	return e - b;
-}
+std::default_random_engine dre;
+std::uniform_int_distribution<> uid{ 1,100 };
+std::uniform_int_distribution<> uidName{ 'a', 'z' };
 
+using namespace std::literals;
+
+struct Dog {
+	std::string name;
+	int n;
+
+	Dog() {
+		n = uid(dre);
+		for (int i = 0; i < 5; ++i)
+			name += uidName(dre);
+	}
+};
 
 // -------
 int main()
 // -------
 {
-	std::vector<int> v{ 1, 2, 3, 4, 5 };
-	
-	// 템플릿 함수 dist를 작성하라
-	std::cout << dist(v.begin(), v.end()) << std::endl;
-	std::cout << dist(v.end(), v.begin()) << std::endl;
+	std::vector<Dog> v(100);
 
-	std::cout << dist(1, 100) << std::endl;
+	for (const Dog& dog : v)
+		std::cout << dog.name << " - " << dog.n << std::endl;
 
-	// 반복자만 함수의 결과를 리턴하는 방법은?
+	std::ranges::sort( v, {}, &Dog::n );
+
+	std::ranges::sort( v, {}, &Dog::name );
+
+	for (const Dog& dog : v)
+		std::cout << dog.name << " - " << dog.n << std::endl;
+
 
 	//save("stl.cpp");
 } 
