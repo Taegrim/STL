cc5aafce47445d2aad33176672e5656e76fb6789
Taegrim <aopo3948@gmail.com>
2022-06-06 13:06:36 +0900

    concept, range
    컨셉 복습
    range(view)

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,56 +1,37 @@
 //-----------------------------------------------------------------------------
-// 2022. 5. 30  월23						월23 수34				 (13주 1일)
+// 2022. 6. 6  월23						월23 수34				 (13주 2일)
 // 
-// C++20의 4가지 핵심 변화
-// range / module / concepts / coroutine
+// Range/View
 // 
-// type-traits
-// range - view
-// 
-// 
-// 6월 6일						수업해야 함
-// 6월 15일 (15주 2일 / 수요일) 기말 시험
-// 6월 20일 (16주 1일 / 월요일) 끝
+// 6월 15일 (15주 1일 / 수요일) 기말 시험
+// 6월 20일 (15주 2일 / 월요일) 끝
 //-----------------------------------------------------------------------------
 #include <iostream>
 #include <ranges>
-#include <random>
+#include <algorithm>
+#include <format>
 #include "save.h"
 #include "STRING.h"
 
-std::default_random_engine dre;
-std::uniform_int_distribution<> uid{ 1,100 };
-std::uniform_int_distribution<> uidName{ 'a', 'z' };
-
-using namespace std::literals;
-
-struct Dog {
-	std::string name;
-	int n;
-
-	Dog() {
-		n = uid(dre);
-		for (int i = 0; i < 5; ++i)
-			name += uidName(dre);
-	}
-};
 
 // -------
 int main()
 // -------
 {
-	std::vector<Dog> v(100);
+	int a[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	for (const Dog& dog : v)
-		std::cout << dog.name << " - " << dog.n << std::endl;
+	// [문제] 1부터 시작하는 자연수에서 홀수만 골라 제곱하여 출력하라
+	// 100개만 출력하라
+	auto odd = [](int n) { return n % 2; };
+	auto POW = [](int n) { return n * n; };
 
-	std::ranges::sort( v, {}, &Dog::n );
-
-	std::ranges::sort( v, {}, &Dog::name );
-
-	for (const Dog& dog : v)
-		std::cout << dog.name << " - " << dog.n << std::endl;
-
-
+	for (int n : std::views::iota(1) 
+			   | std::views::filter( odd ) 
+			   | std::views::transform(POW) 
+			   | std::views::take(1000)
+		)
+		std::cout << std::format("{:10}", n);
+	std::cout << std::endl;
+	
 	//save("stl.cpp");
 } 
