94cc75f4ca71d5428c3174d8e7fcb2078455ec2c
Taegrim <aopo3948@gmail.com>
2022-06-09 22:39:10 +0900

    Anagram, ranges
    Anagram 찾기
    1. 순열을 이용한 찾기
     -> 단어의 글자 개수가 많아지면 실행 시간이 매우 길어짐
    2. 단어를 정렬하고 비교
     -> 애너그램은 정렬 한 결과가 같은지 확인하면 됨

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,37 +1,41 @@
 //-----------------------------------------------------------------------------
-// 2022. 6. 6  월23						월23 수34				 (13주 2일)
+// 2022. 6. 8  수34						월23 수34				 (14주 1일)
 // 
-// Range/View
+// anagram 찾기
 // 
 // 6월 15일 (15주 1일 / 수요일) 기말 시험
 // 6월 20일 (15주 2일 / 월요일) 끝
 //-----------------------------------------------------------------------------
 #include <iostream>
-#include <ranges>
+#include <fstream>
+#include <vector>
 #include <algorithm>
-#include <format>
+#include <ranges>
 #include "save.h"
 #include "STRING.h"
 
+struct WP : std::pair<std::string, std::string> {		// word pair 
+	WP(std::string s) : std::pair<std::string, std::string> { s, s }{
+		std::sort(first.begin(), first.end());
+	}
+};
 
 // -------
 int main()
 // -------
 {
-	int a[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-	// [문제] 1부터 시작하는 자연수에서 홀수만 골라 제곱하여 출력하라
-	// 100개만 출력하라
-	auto odd = [](int n) { return n % 2; };
-	auto POW = [](int n) { return n * n; };
-
-	for (int n : std::views::iota(1) 
-			   | std::views::filter( odd ) 
-			   | std::views::transform(POW) 
-			   | std::views::take(1000)
-		)
-		std::cout << std::format("{:10}", n);
-	std::cout << std::endl;
-	
 	//save("stl.cpp");
+
+	std::ifstream in{ "단어들.txt" };	// 단어 수 369882
+	
+	std::vector<WP> v{std::istream_iterator<std::string>{in}, {}};
+	std::cout << "단어 수 - " << v.size() << std::endl;
+	
+	std::ranges::sort(v, {}, &WP::first);
+
+	for (WP words : v | std::views::drop(1'000)
+					  | std::views::take(50))
+		std::cout << words.first << " --- " << words.second << std::endl;
+
+	
 } 
