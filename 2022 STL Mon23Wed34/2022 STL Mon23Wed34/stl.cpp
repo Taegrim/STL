f967a70b4ae9209737142424c542466714b948fe
Taegrim <aopo3948@gmail.com>
2022-06-13 22:29:21 +0900

    Anagram Pair, length sort
    애너그램쌍 구하기,
    애너그램 쌍에서 쌍의 개수가 많은 것 찾기
    애너그램 쌍에서 단어가 가장 긴 것 찾기

--- 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
+++ 2022 STL Mon23Wed34/2022 STL Mon23Wed34/stl.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// 2022. 6. 8  수34						월23 수34				 (14주 1일)
+// 2022. 6. 13  월23						월23 수34				 (14주 2일)
 // 
 // anagram 찾기
 // 
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <ranges>
 #include "save.h"
@@ -19,6 +20,7 @@ struct WP : std::pair<std::string, std::string> {		// word pair
 		std::sort(first.begin(), first.end());
 	}
 };
+
 
 // -------
 int main()
@@ -33,9 +35,62 @@ int main()
 	
 	std::ranges::sort(v, {}, &WP::first);
 
-	for (WP words : v | std::views::drop(1'000)
-					  | std::views::take(50))
+	for (const WP& words : v | std::views::drop(10'000) 
+							 | std::views::take(200))
 		std::cout << words.first << " --- " << words.second << std::endl;
 
+	// [문제] 전체 anagram 쌍을 출력하라
+
+	// 현재 WP와 다음 WP에서 first가 같니?  - adjacent_find()
+	// 같은 위치를 발견했다면
+	// 달라지는 곳은 어디?
+
+
+	// [문제] 전체 anagram 쌍을 개수 기준 내림차순으로 출력하라.
+
+	auto b = v.cbegin();
+	int cnt{};
+
+	std::vector<std::list<std::string>> vls;
+
+	while (true) {
+		auto i = std::adjacent_find(b, v.cend(), [](const WP& a, const WP& b) {
+			return a.first == b.first;
+			});
+		
+		if (i == v.cend())
+			break;
+
+		auto j = std::find_if_not(i + 1, v.cend(), [i](const WP& a) {
+			return a.first == i->first;
+			});
+		
+		// [i, j) 구간은 서로 anagram 관계이다.
+		std::list<std::string> ls;
+
+		
+		for (auto p = i; p != j; ++p)
+			ls.push_back(p->second);
+		vls.push_back(ls);
+
+		b = j;
+	}
+
+	// vector<list<string>> 단어 길이가 긴 거 우선
+	std::sort(vls.begin(), vls.end(),
+		[](const std::list<std::string>& a, const std::list<std::string>& b) {
+			return a.begin()->size() > b.begin()->size();
+		});
 	
+	std::cout << "가장 긴 단어 - " << vls[0].begin()->size() << std::endl;
+
+	//앞에서 부터 10개 출력
+
+	for (const std::list<std::string>& ls : vls | std::views::take(2000)) {
+		for (const std::string& s : ls)
+			std::cout << s << " ";
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
 } 
